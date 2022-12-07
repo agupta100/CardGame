733d2e298de291a6b5dc270189122900f37e13b8
agupta100 <aarav.gupta@menloschool.org>
2022-12-07 10:03:37 -0800

    Debugged Game class.

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,7 @@ public class Game
         String[] suits = {"King", "Queen", "Jack", "Queen"};
         int[] points = {7, 8, 9};
         Deck test2 = new Deck(ranks, suits, points);
-//        test2.shuffle();
+        test2.shuffle();
         String suit = test.getSuit();
         System.out.println(suit);
 
